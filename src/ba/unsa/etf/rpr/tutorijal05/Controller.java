b607faf292d5e16d0161887b62f2d8c105ca8990
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2018-11-14 21:39:38 +0100

    klasa Controller

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -7,6 +7,29 @@ import javafx.scene.control.Label;
 
 public class Controller {
 
+    public boolean decimalni = false;
+    private boolean jednako=false;
+    public Label display;
+    public Button btn0;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+    public Button btn5;
+    public Button btn6;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+    public Button dotBtn;
+    public Button plusBtn;
+    public Button productBtn;
+    public Button equalsBtn;
+    public Button percentBtn;
+    private String prviBroj="";
+    private String drugiBroj="";
+    private String minus="";
+    private String product="";
+    private  String divide="";
     private SimpleStringProperty tekst;
 
     public Controller() {
@@ -23,6 +46,33 @@ public class Controller {
 
     public void setTekst(String tekst) {
         this.tekst.set(tekst);
+    }
+
+    void DodajCifru(int broj) {
+        if (tekst.get().equals("0") && tekst.get().length() == 1) {
+            if(broj==0)tekst.set(btn0.getText());
+            else if (broj == 1) tekst.set(btn1.getText());
+            else if (broj == 2) tekst.set(btn2.getText());
+            else if (broj == 3) tekst.set(btn3.getText());
+            else if (broj == 4) tekst.set(btn4.getText());
+            else if (broj == 5) tekst.set(btn5.getText());
+            else if (broj == 6) tekst.set(btn6.getText());
+            else if (broj == 7) tekst.set(btn7.getText());
+            else if (broj == 8) tekst.set(btn8.getText());
+            else tekst.set(btn9.getText());
+        } else {
+            if (broj==0 ) tekst.set(tekst.get() + btn0.getText());
+            else if (broj == 1) tekst.set(tekst.get() + btn1.getText());
+            else if (broj == 2) tekst.set(tekst.get() + btn2.getText());
+            else if (broj == 3) tekst.set(tekst.get() + btn3.getText());
+            else if (broj == 4) tekst.set(tekst.get() + btn4.getText());
+            else if (broj == 5) tekst.set(tekst.get() + btn5.getText());
+            else if (broj == 6) tekst.set(tekst.get() + btn6.getText());
+            else if (broj == 7) tekst.set(tekst.get() + btn7.getText());
+            else if (broj == 8) tekst.set(tekst.get() + btn8.getText());
+            else tekst.set(tekst.get() + btn9.getText());
+        }
+
     }
 
 }
