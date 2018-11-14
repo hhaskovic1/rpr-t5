01a463557308193e49efbc224a1b8f9458a39f4d
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2018-11-14 21:36:29 +0100

    prvi commit

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,16 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    private SimpleStringProperty tekst;
+
+    public Controller() {
+        tekst = new SimpleStringProperty("0");
+    }
 
 }
